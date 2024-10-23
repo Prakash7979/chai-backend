eb82dda28aeacedc84f8c1ca9f7e9d39fcecb610
Prakash7979 <prakash.bhusal7799@gmail.com>
2024-10-23 08:48:35 +0545

    register, postman, access/refresh token

--- src/app.js
+++ src/app.js
@@ -2,19 +2,28 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express;
+const app = express();
 
-app.request(
+app.use(
   cors({
-    origin: Process.env.CORS_ORIGIN,
-    credential: true,
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
   })
 );
 
-app.request(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
 
 export { app };
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -15,7 +15,7 @@ class ApiError extends Error {
     if (statck) {
       this.stack = statck;
     } else {
-      Error.captureStackTrece(this, this.constructor);
+      Error.captureStackTrace(this, this.constructor);
     }
   }
 }
